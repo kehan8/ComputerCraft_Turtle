a72c9ef13d1ed2078fbdc2ffb3a5ec806fb8bfe1
kehan8 <ks.davet@gmail.com>
2026-08-06 08:15:37 +0200

    fixed carrot

--- carrot_auto_farm.lua
+++ carrot_auto_farm.lua
@@ -1,40 +1,40 @@
-    turtle.select(1)
-    homing = false
-    start = false
+turtle.select(1)
+homing = false
+start = false
 
-    while true do
-        sleep(0.1)
-        local chest_is_full = false
+while true do
+    sleep(0.1)
+    local chest_is_full = false
 
-        for i = 2, 16 do
-            if turtle.getItemCount(i) > 0 then
-                turtle.select(i)
-                local item = turtle.getItemDetail()
+    for i = 2, 16 do
+        if turtle.getItemCount(i) > 0 then
+            turtle.select(i)
+            local item = turtle.getItemDetail()
 
-                if item then
-                    if item.name == "minecraft:carrot" then
-                        if not turtle.dropUp() then
-                            chest_is_full = true
-                        end
-                    else
-                        turtle.dropDown()
+            if item then
+                if item.name == "minecraft:carrot" then
+                    if not turtle.dropUp() then
+                        chest_is_full = true
                     end
+                else
+                    turtle.dropDown()
                 end
             end
         end
-        turtle.select(1)
+    end
+    turtle.select(1)
 
-        if chest_is_full then
-            term.clear()
-            term.setCursorPos(1,1)
-            print("chest is full - waiting...")
-            sleep(2)
-        else
-            term.clear()
-            term.setCursorPos(1,1)
-            print("chest has space - farming")
+    if chest_is_full then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest is full - waiting...")
+        sleep(2)
+    else
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest has space - farming")
 
-            for side = 1, 4 do
+        for side = 1, 4 do
             local is_block, blockdata = turtle.inspect()
             if is_block then
                 if blockdata.state.age == 7 then
