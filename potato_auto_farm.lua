b6d3ca7d6fb4ccdd20672cfb60146ba8a2a84348
kehan8 <ks.davet@gmail.com>
2026-08-06 07:29:56 +0200

    fixed tab \t

--- potato_auto_farm.lua
+++ potato_auto_farm.lua
@@ -3,51 +3,51 @@ homing = false
 start = false
 
 while true do
-   sleep(0.1)
-   local chest_is_full = false
-   for i = 2, 16 do
-       if turtle.getItemCount(i) > 0 then
-           turtle.select(i)
-           local item = turtle.getItemDetail()
+	sleep(0.1)
+	local chest_is_full = false
+	for i = 2, 16 do
+		if turtle.getItemCount(i) > 0 then
+			turtle.select(i)
+			local item = turtle.getItemDetail()
 
-           if item then
-               if item.name == "minecraft:potato" then
-                   if not turtle.dropUp() then
-                       chest_is_full = true
-                   end 
-               else
-                   turtle.dropDown()
-               end
-           end
-       end
-   end
-   turtle.select(1)
+			if item then
+				if item.name == "minecraft:potato" then
+					if not turtle.dropUp() then
+						chest_is_full = true
+					end 
+				else
+					turtle.dropDown()
+				end
+			end
+		end
+	end
+	turtle.select(1)
 
-   if chest_is_full then
-       term.clear()
-       term.setCursorPos(1,1)
-       print("chest is full - waiting...")
-       sleep(2)
-   else
-       term.clear()
-       term.setCursorPos(1,1)
-       print("chest has space - farming")
-       local is_block, blockdata = turtle.inspect()
-       if is_block then
-            if blockdata.state.age == 7 then
-                turtle.dig()
-                turtle.place() 
-            end
-       end
-       turtle.turnLeft()
-       local is_block, blockdata = turtle.inspect()
+	if chest_is_full then
+		term.clear()
+		term.setCursorPos(1,1)
+		print("chest is full - waiting...")
+		sleep(2)
+	else
+		term.clear()
+		term.setCursorPos(1,1)
+		print("chest has space - farming")
+		local is_block, blockdata = turtle.inspect()
+		if is_block then
+				if blockdata.state.age == 7 then
+					turtle.dig()
+					turtle.place() 
+				end
+		end
+		turtle.turnLeft()
+		local is_block, blockdata = turtle.inspect()
 
-       if is_block then
-           if blockdata.state.age == 7 then
-               turtle.dig()
-               turtle.place() 
-           end
-       end
-       turtle.turnLeft() 
-   end
+		if is_block then
+			if blockdata.state.age == 7 then
+				turtle.dig()
+				turtle.place() 
+			end
+		end
+		turtle.turnLeft() 
+	end
 end
--- tumblestone_auto_farm.lua
+++ tumblestone_auto_farm.lua
@@ -29,79 +29,79 @@ local TUMBLESTONE_FULLGROWN = {
 }
 
 local function selectTumblestone()
-    for i = 1, 16 do
-        local item = turtle.getItemDetail(i)
-        if item and TUMBLESTONE_ITEM[item.name] then
-            turtle.select(i)
-            return true
-        end
-    end
-    return false
+	for i = 1, 16 do
+		local item = turtle.getItemDetail(i)
+		if item and TUMBLESTONE_ITEM[item.name] then
+			turtle.select(i)
+			return true
+		end
+	end
+	return false
 end
 
 turtle.select(1)
 homing = false
 
 while not homing do
-    local is_block, blockdata = turtle.inspect()
-    if is_block then
-        if TUMBLESTONE_STAGES[blockdata.name] then
-            turtle.turnLeft()
-            local is_block2, blockdata2 = turtle.inspect()
-            if is_block2 then
-                if TUMBLESTONE_STAGES[blockdata2.name] then
-                    turtle.turnRight()
-                    homing = true
-                end
-            else
-                turtle.turnRight()
-                turtle.turnRight()
-                homing = true
-            end
-        else
-            print("not found")
-            turtle.turnLeft()
-        end
-    else
+	local is_block, blockdata = turtle.inspect()
+	if is_block then
+		if TUMBLESTONE_STAGES[blockdata.name] then
+			turtle.turnLeft()
+			local is_block2, blockdata2 = turtle.inspect()
+			if is_block2 then
+				if TUMBLESTONE_STAGES[blockdata2.name] then
+					turtle.turnRight()
+					homing = true
+				end
+			else
+				turtle.turnRight()
+				turtle.turnRight()
+				homing = true
+			end
+		else
+			print("not found")
+			turtle.turnLeft()
+		end
+	else
 		print("not found")
-        turtle.turnLeft()
-    end
+		turtle.turnLeft()
+	end
 end
 
 while true do
-    sleep(0.1)
-    local chest_is_full = false
+	sleep(0.1)
+	local chest_is_full = false
 
-    for i = 1, 16 do
-        if turtle.getItemCount(i) > 0 then
-            turtle.select(i)
-            local item = turtle.getItemDetail()
-            if item and TUMBLESTONE_ITEM[item.name] then
-                if not turtle.dropUp() then
-                    chest_is_full = true
-                end
-            end
-        end
-    end
-    turtle.select(1)
+	for i = 1, 16 do
+		if turtle.getItemCount(i) > 0 then
+			turtle.select(i)
+			local item = turtle.getItemDetail()
+			if item and TUMBLESTONE_ITEM[item.name] then
+				if not turtle.dropUp() then
+					chest_is_full = true
+				end
+			end
+		end
+	end
+	turtle.select(1)
 
-    term.clear()
-    term.setCursorPos(1,1)
+	term.clear()
+	term.setCursorPos(1,1)
 
-    if chest_is_full then
-        print("chest is full - waiting...")
-        sleep(2)
-    else
-        print("chest has space - mining")
-        for side = 1, 4 do
-            local is_block, blockdata = turtle.inspect()
-            if is_block and TUMBLESTONE_FULLGROWN[blockdata.name] then
-                turtle.dig()
-                if selectTumblestone() then
-                    turtle.place()
-                end
-            end
-            turtle.turnRight()
-        end
-    end
+	if chest_is_full then
+		print("chest is full - waiting...")
+		sleep(2)
+	else
+		print("chest has space - mining")
+		for side = 1, 4 do
+			local is_block, blockdata = turtle.inspect()
+			if is_block and TUMBLESTONE_FULLGROWN[blockdata.name] then
+				turtle.dig()
+				if selectTumblestone() then
+					turtle.place()
+				end
+			end
+			turtle.turnRight()
+		end
+	end
 end
