e23e9426161c7e97af3fb3e1af950c2d1cb3cf85
kehan8 <ks.davet@gmail.com>
2026-08-06 05:32:10 +0200

    updated wheat auto farm for 4 sides

--- wheat_auto_farm.lua
+++ wheat_auto_farm.lua
@@ -3,81 +3,75 @@ homing = false
 start = false
 
 while not homing do
-	local is_block, blockdata = turtle.inspect()
-	if is_block then
-		if blockdata.name == "minecraft:wheat" then
-			turtle.turnLeft()
-			local is_block, blockdata = turtle.inspect()
-			if is_block then
-				if blockdata.name == "minecraft:wheat" then
-				turtle.turnRight()
-				homing = true
-				end
-			else 
-				turtle.turnRight()
-				turtle.turnRight()
-				homing = true
-			end
-		else
-			print("not found")
-			turtle.turnLeft()
-		end
-	else 
-		turtle.turnLeft()
-	end
+    local is_block, blockdata = turtle.inspect()
+    if is_block then
+        if blockdata.name == "minecraft:wheat" then
+            turtle.turnLeft()
+            local is_block2, blockdata2 = turtle.inspect()
+            if is_block2 then
+                if blockdata2.name == "minecraft:wheat" then
+                    turtle.turnRight()
+                    homing = true
+                end
+            else
+                turtle.turnRight()
+                turtle.turnRight()
+                homing = true
+            end
+        else
+            print("not found")
+            turtle.turnLeft()
+        end
+    else
+        print("not found")
+        turtle.turnLeft()
+    end
 end
 
 while true do
-sleep(0.1)
-local chest_is_full = false
+    sleep(0.1)
+    local chest_is_full = false
 
-for i = 2, 16 do
-	if turtle.getItemCount(i) > 0 then
-		turtle.select(i)
-		local item = turtle.getItemDetail()
-		
-		if item then
-			if item.name == "minecraft:wheat" then
-				if not turtle.dropUp() then
-					chest_is_full = true
-				end
-			elseif item.name == "minecraft:wheat_seeds" then
-				if not turtle.transferTo(1) then
-					turtle.dropDown()
-				end
-			else
-				turtle.dropDown()
-			end
-		end
-	end
-end
-turtle.select(1)
+    for i = 2, 16 do
+        if turtle.getItemCount(i) > 0 then
+            turtle.select(i)
+            local item = turtle.getItemDetail()
+            if item then
+                if item.name == "minecraft:wheat" then
+                    if not turtle.dropUp() then
+                        chest_is_full = true
+                    end
+                elseif item.name == "minecraft:wheat_seeds" then
+                    if not turtle.transferTo(1) then
+                        turtle.dropDown()
+                    end
+                else
+                    turtle.dropDown()
+                end
+            end
+        end
+    end
+    turtle.select(1)
 
-if chest_is_full then
-	term.clear()
-	term.setCursorPos(1,1)
-	print("chest is full - waiting...")
-	sleep(2)
-else
-	term.clear()
-	term.setCursorPos(1,1)
-	print("chest has space - farming")
-	local is_block, blockdata = turtle.inspect()
-	if is_block then
-			if blockdata.state.age == 7 then
-				turtle.dig()
-				turtle.place() 
-			end
-	end
-	turtle.turnLeft()
-	local is_block, blockdata = turtle.inspect()
+    if chest_is_full then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest is full - waiting...")
+        sleep(2)
+    else
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest has space - farming")
 
-	if is_block then
-		if blockdata.state.age == 7 then
-			turtle.dig()
-			turtle.place() 
-		end
-	end
-	turtle.turnRight()  
-end
+        for side = 1, 4 do
+            local is_block, blockdata = turtle.inspect()
+            if is_block then
+                if blockdata.state.age == 7 then
+                    turtle.dig()
+                    turtle.place()
+                end
+            end
+            turtle.turnRight()
+        end
+    end
 end
