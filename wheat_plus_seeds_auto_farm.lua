4f1f10feb32f5dfe32c5b711309d0dfd470176f0
kehan8 <ks.davet@gmail.com>
2026-09-02 12:08:40 +0200

    added wheat + seeds auto farm

---
+++ wheat_plus_seeds_auto_farm.lua
@@ -0,0 +1,129 @@
+local function selectSeed()
+    for i = 1, 16 do
+        local item = turtle.getItemDetail(i)
+        if item and item.name == "minecraft:wheat_seeds" then
+            turtle.select(i)
+            return true
+        end
+    end
+    return false
+end
+
+local SEED_BUFFER_SLOT = 2
+
+local function inventoryIsFull()
+    for i = 1, 16 do
+        if turtle.getItemCount(i) == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+local function dumpOverflowBack()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for i = 1, 16 do
+        if i ~= SEED_BUFFER_SLOT and turtle.getItemCount(i) > 0 then
+            turtle.select(i)
+            turtle.drop()
+        end
+    end
+    turtle.select(1)
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+turtle.select(1)
+homing = false
+start = false
+
+while not homing do
+    local is_block, blockdata = turtle.inspect()
+    if is_block then
+        if blockdata.name == "minecraft:wheat" then
+            turtle.turnLeft()
+            local is_block2, blockdata2 = turtle.inspect()
+            if is_block2 then
+                if blockdata2.name == "minecraft:wheat" then
+                    turtle.turnRight()
+                    homing = true
+                end
+            else
+                turtle.turnRight()
+                turtle.turnRight()
+                homing = true
+            end
+        else
+            print("not found")
+            turtle.turnLeft()
+        end
+    else
+        print("not found")
+        turtle.turnLeft()
+    end
+end
+
+while true do
+    sleep(0.1)
+    local chest_above_full = false
+    local chest_below_full = false
+
+    for i = 1, 16 do
+        if turtle.getItemCount(i) > 0 then
+            turtle.select(i)
+            local item = turtle.getItemDetail()
+            if item then
+                if item.name == "minecraft:wheat" then
+                    if not turtle.dropUp() then
+                        chest_above_full = true
+                    end
+                elseif item.name == "minecraft:wheat_seeds" then
+                    if i ~= SEED_BUFFER_SLOT then
+                        turtle.transferTo(SEED_BUFFER_SLOT)
+                        if turtle.getItemCount(i) > 0 then
+                            if not turtle.dropDown() then
+                                chest_below_full = true
+                            end
+                        end
+                    end
+                else
+                    if not turtle.dropDown() then
+                        chest_below_full = true
+                    end
+                end
+            end
+        end
+    end
+    turtle.select(1)
+
+    if inventoryIsFull() then
+        dumpOverflowBack()
+    end
+
+    local chest_is_full = chest_above_full and chest_below_full
+
+    if chest_is_full then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest is full - waiting...")
+        sleep(2)
+    else
+        term.clear()
+        term.setCursorPos(1,1)
+        print("chest has space - farming")
+
+        for side = 1, 4 do
+            local is_block, blockdata = turtle.inspect()
+            if is_block then
+                if blockdata.state.age == 7 then
+                    turtle.dig()
+                    if selectSeed() then
+                        turtle.place()
+                    end
+                end
+            end
+            turtle.turnRight()
+        end
+    end
+end
